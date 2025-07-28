0451391f1a8ec5e7807e2dad532ed2f29c84c7f9
aydie <aydieprodstudio@gmail.com>
2025-07-29 00:09:33 +0530

    Updated Utils, Exception, Constants and Version Details

--- aydie_genai/__init__.py
+++ aydie_genai/__init__.py
@@ -1,1 +1,11 @@
-This file makes the directory a Python package.
+from aydie_genai.constants import VERSION
+
+__version__ = VERSION
+
+from . import genai
+
+__all__ = [
+    'genai',
+]
+
+print("aydie_genai package loaded successfully.")
